d64c601e7edbd48da33e6c2da5ef6549587d45cb
moffd234 <moffd234@gmail.com>
2024-03-05 14:25:40 -0500

    Completed getRow(numberOfStars)

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -1,5 +1,7 @@
 package io.zipcoder.microlabs.mastering_loops;
 
+import java.util.Arrays;
+
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
@@ -14,11 +16,24 @@ public class TriangleUtilities {
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String output = "";
+        for(int i = 0; i < numberOfStars; i++){
+            output += "*";
+        }
+
+        return output;
     }
 
     public static String getSmallTriangle() {
-        return null;
+        String triangle = "";
+        int numberOfRows = 3;
+        for(int i = 1; i < numberOfRows; i++){
+            for(int j = 0; j < i; j++) {
+                triangle += "*";
+            }
+            triangle += "\n";
+        }
+        return triangle;
     }
 
     public static String getLargeTriangle() {
